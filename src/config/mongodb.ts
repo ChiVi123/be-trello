47078bc115c453e71d8ea124db469a323f55ade2
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-01-10 17:14:43 +0700

    done alias import for mode dev and product

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,10 +1,7 @@
 import { Db, MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~config/environment";
 
-const MONGO_PASSWORD = "PQIxWsMkc7vMAxm2";
-const MONGO_URI = `mongodb+srv://vinghcit1811:${MONGO_PASSWORD}@cluster0.mrncr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const MONGO_DATABASE_NAME = "trello-db";
-
-const mongoClientInstance = new MongoClient(MONGO_URI, {
+const mongoClientInstance = new MongoClient(env.MONGO_URI, {
     serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
 });
 
@@ -12,7 +9,7 @@ let mongoDatabaseInstance: Db | null = null;
 
 export const connectDB = async () => {
     await mongoClientInstance.connect();
-    mongoDatabaseInstance = mongoClientInstance.db(MONGO_DATABASE_NAME);
+    mongoDatabaseInstance = mongoClientInstance.db(env.MONGO_DATABASE_NAME);
 };
 export const getDB = () => {
     if (!mongoDatabaseInstance) throw new Error("Must connect to Database first");
